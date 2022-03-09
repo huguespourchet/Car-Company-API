fd9d7a2646b6257cb4b5114925c079da5a5e854d
Hugues <huguespourchet@gmail.com>
2022-03-09 23:20:18 +0100

    #2 Doc requêtes offices et orders

--- routes/offices.route.js
+++ routes/offices.route.js
@@ -4,11 +4,233 @@ const customersController = require("../controllers/customers.controller");
 let router = express.Router();
 
 router.get("/list",officesController.getall);
+/**
+ * @swagger
+ * /offices/list:
+ *   get:
+ *      description: Used to get all offices
+ *      tags:
+ *          - offices
+ *      responses:
+ *          '200':
+ *              description: Resource returned successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.post("/count",officesController.nboffices);
+/**
+ * @swagger
+ * /offices/count:
+ *   post:
+ *      description: Used to count the number of offices
+ *      tags:
+ *          - offices
+ *      responses:
+ *          '200':
+ *              description: Resource returned successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.put("/list/:id/employee",officesController.getemployeefromeofficeid);
+/**
+ * @swagger
+ * /offices/list/{id}/employee:
+ *  put:
+ *      description: Used to get employees that works on an office
+ *      tags:
+ *          - offices
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: office's id
+ *            required: true;
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.post("/add",officesController.add);
+/**
+ * @swagger
+ * /offices/add:
+ *   post:
+ *      description: Used to add a new office
+ *      tags:
+ *          - offices
+ *      parameters:
+ *          - in: body
+ *            name: office
+ *            description: office data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - officeCode
+ *                 - city
+ *                 - phone
+ *                 - addressLine1
+ *                 - addressLine2
+ *                 - state
+ *                 - country
+ *                 - postalCode
+ *                 - territory
+ *              properties:
+ *                  officeCode:
+ *                      type: integer
+ *                  city:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: Belfort
+ *                  phone:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: +33 14 723 4404
+ *                  addressLine1:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: 1550 Court Place
+ *                  addressLine2:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: Suite 300
+ *                  state:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: NULL
+ *                  country:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: France
+ *                  postalCode:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: 25000
+ *                  territory:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: EMEA
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 router.put("/edit/:id",officesController.edit);
+/**
+ * @swagger
+ * /offices/edit/{id}:
+ *   put:
+ *      description: Used to edit an office
+ *      tags:
+ *          - offices
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: office code
+ *            required: true
+ *          - in: body
+ *            name: office
+ *            description: office data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - city
+ *                 - phone
+ *                 - addressLine1
+ *                 - addressLine2
+ *                 - state
+ *                 - country
+ *                 - postalCode
+ *                 - territory
+ *              properties:
+ *                  city:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: chnagedCity
+ *                  phone:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: +33 14 723 4404
+ *                  addressLine1:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: changeAddress
+ *                  addressLine2:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: Suite 300
+ *                  state:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: NULL
+ *                  country:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: France
+ *                  postalCode:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: 25000
+ *                  territory:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 50
+ *                      example: EMEA
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.delete("/:id/delete",officesController.delete);
+/**
+ * @swagger
+ * /offices/{id}/delete:
+ *   delete:
+ *      description: Used to delete an office
+ *      tags:
+ *          - offices
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: string
+ *            description: officeCode
+ *            required: true
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 module.exports = router;
--- routes/orders.route.js
+++ routes/orders.route.js
@@ -3,8 +3,156 @@ const express = require("express");
 let router = express.Router();
 
 router.put("/customer/:id/last",orderController.getLastOrderById);
+/**
+ * @swagger
+ * /orders/customer/{id}/last:
+ *   put:
+ *      description: Used to get the last order's details of a customer
+ *      tags:
+ *          - orders
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: order's id
+ *            required: true
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.get("/id/:id",orderController.getallproductsfromorder);
+/**
+ * @swagger
+ * /orders/id/{id}:
+ *   get:
+ *      description: Used to get all products from an order
+ *      tags:
+ *          - orders
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: order's id
+ *            required: true
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.post("/id/:id/add",orderController.addProductToOrder);
+/**
+ * @swagger
+ * /orders/id/{id}/add:
+ *   post:
+ *      description: Used to register a product to an order
+ *      tags:
+ *          - orders
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: order's id
+ *            required: true
+ *          - in: body
+ *            name: Product
+ *            description: orderDetails data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - productCode
+ *                 - quantityOrdered
+ *                 - priceEach
+ *                 - orderLineNumber
+ *              properties:
+ *                  productCode:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 15
+ *                      example: S700_2824
+ *                  quantityOrdered:
+ *                      type: integer
+ *                      example: 25
+ *                  priceEach:
+ *                      type: decimal
+ *                      example: 131.49
+ *                  orderLineNumber:
+ *                      type: integer
+ *                      example: 12
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.delete("/id/:id/product/:idp/delete",orderController.deleteProductFromOrder);
+/**
+ * @swagger
+ * /orders/id/{id}/product/{idp}/delete:
+ *   delete:
+ *      description: Used to delete a product from an order
+ *      tags:
+ *          - orders
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: order's ID
+ *            required: true
+ *          - in: path
+ *            name: idp
+ *            type: string
+ *            minLength: 1
+ *            maxLength: 50
+ *            description: product's code
+ *            example: S700_2824
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 router.put("/id/:id/product/:idp/updateQtt/:nb",orderController.updateQuantityOrdered);
+/**
+ * @swagger
+ * /orders/id/{id}/product/{idp}/updateQtt/{nb}:
+ *   put:
+ *      description: Used to update quantity of a product ordered
+ *      tags:
+ *          - orders
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            description: order's ID
+ *            required: true
+ *          - in: path
+ *            name: idp
+ *            type: string
+ *            minLength: 1
+ *            maxLength: 50
+ *            description: product's code
+ *            example: S700_2824
+ *          - in: path
+ *            name: nb
+ *            type: integer
+ *            description: new quantity
+ *      responses:
+ *          '200':
+ *              description: Resource updated successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 module.exports = router;
